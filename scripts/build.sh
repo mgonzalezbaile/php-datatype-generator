df0e50d50e76aa9bcfd217fa7e7682e9975478da
Maikel González Baile <m.gonzalezbaile@gmail.com>
2018-05-29 10:09:01 +0200

    Fix Travis CI (#1)

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,4 +4,4 @@ set -ev
 
 docker build . -t pdg
 docker run pdg curl -s http://getcomposer.org/installer | php
-docker run -ti --rm -u $UID:$UID -e COMPOSER_HOME=/application/.composer -v $(pwd):/application pdg php composer.phar install --no-dev
+docker run -ti --rm -u $UID:$UID -e COMPOSER_HOME=/application/.composer -v $(pwd):/application pdg php composer.phar install
